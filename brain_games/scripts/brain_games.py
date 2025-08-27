db9117c4b0ba44d02a2f6dd7991834b960637edf
Coverfog <Hain.Heither@gmail.com>
2025-08-27 18:30:10 +0300

    Add welcoming message

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,10 @@
+from brain_games.cli import welcome_user
+
+
 def main():
     print("Welcome to the Brain Games!")
+    user = welcome_user()
+    print(f"Hello, {user}!")
 
 if __name__ == '__main__':
     main()
