9684aeb9c795fcc17bfb30b349cef71871c20a00
Coverfog <Hain.Heither@gmail.com>
2025-08-28 18:03:41 +0300

    Update calc_game.py: rework inner logic

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -3,23 +3,24 @@ from random import choice, randint
 
 def calc_game():
     start_sentence = "What is the result of the expression?"
-    actions = ['+', '-', '*']
-    action = choice(actions)
+    operators = ['+', '-', '*']
+    operator = choice(operators)
 
-    if action == '+':
-        num1 = randint(1, 20)
-        num2 = randint(1, 20)
-        result = num1 + num2
-    elif action == '-':
-        num1 = randint(10, 20)
-        num2 = randint(1, 10)
-        result = num1 - num2
-    elif action == '*':
-        num1 = randint(1, 20)
-        num2 = randint(2, 5)
-        result = num1 * num2
+    match operator:
+        case '+':
+            num1 = randint(1, 20)
+            num2 = randint(1, 20)
+            result = num1 + num2
+        case '-':
+            num1 = randint(10, 20)
+            num2 = randint(1, 10)
+            result = num1 - num2
+        case '*':
+            num1 = randint(1, 20)
+            num2 = randint(2, 5)
+            result = num1 * num2
 
-    question = f"Question: {str(num1)} {action} {str(num2)}"
+    question = f"Question: {str(num1)} {operator} {str(num2)}"
     correct_answer = str(result)
 
     return start_sentence, question, correct_answer
