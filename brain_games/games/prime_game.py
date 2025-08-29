f48c6f42d275271817f40b5f081a24ed737af170
Coverfog <Hain.Heither@gmail.com>
2025-08-29 13:05:06 +0300

    Add prime-game

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+def prime_game():
+    start_sentence = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    num = randint(0, 100)
+
+    question = f"Question: {num}"
+    correct_answer = is_prime(num)
+
+    return start_sentence, question, correct_answer
+
+
+def is_prime(num):
+
+    if num <= 1:
+        return 'no'
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return 'no'
+
+    return 'yes'
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.games.prime_game import prime_game
+from brain_games.games_engine import start_game
+
+
+def main():
+    start_game(prime_game)
+
+
+if __name__ == '__main__':
+    main()
